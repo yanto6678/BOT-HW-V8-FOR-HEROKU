586dd239b2c72952661cfebb6dbee59f73611bbc
yanto6678 <99623377+yanto6678@users.noreply.github.com>
2022-09-29 08:06:08 +0700

    Update hwkal.js

--- hwkal.js
+++ hwkal.js
@@ -13,13 +13,13 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 //=================================================//
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = 'https://youtube.com/c/HwModsWa857'
-global.ig = 'https://www.instagram.com/hikal_857/?hl=id'
-global.mygc = 'wa.me/6285714170944'
-global.myweb = 'https://eclass.iainsalatiga.ac.id/app/upload/users/1/10892/my_files/XhiroMhonshine.html'
-global.linkgrupss = "https://chat.whatsapp.com/Jh7zaCSnPCBC4gdYOEVm0K"
-global.linkpro = 'mailto:support@whatsapp.com?subject=Deactivate%20My%20Account%20Number&body=Please%20Deactivate%20The%20My%20Account%20Number%20%2B62XXXXXXXX%20Immediately%20Because%20The%20Number%20Has%20Been%20Lost'
-global.email = 'denyp857@gmail.com'
+global.youtube = 'https://wa.me/6283156715878'
+global.ig = 'https://wa.me/6283156715878'
+global.mygc = 'https://wa.me/6283156715878'
+global.myweb = 'https://wa.me/6283156715878'
+global.linkgrupss = "https://wa.me/6283156715878"
+global.linkpro = 'https://wa.me/6283156715878'
+global.email = 'Arthur@gmail.com'
 global.region = 'indonesia'
 //—————「 Set Nama Own & Bot 」—————//
 global.ownername = 'HW MODS WA' //ubah jadi nama mu, note tanda ' gausah di hapus!
@@ -27,11 +27,11 @@ global.botname = 'HW MODS WA' //ubah jadi nama bot mu, note tanda ' gausah di ha
 global.footer = 'HW MODS WA' //ubah jadi nama mu, note tanda ' gausah di hapus!
 //=================================================//
 // Other
-global.owner = ['6281214281312']
-global.premium = ['6281214281312']
-global.packname = 'HW MODS WA'
-global.ownerr = ['HW MODS WA']
-global.author = 'HW MODS WA'
+global.owner = ['6283156715878']
+global.premium = ['6283156715878']
+global.packname = 'Arthur Bot'
+global.ownerr = ['Arthur']
+global.author = '2022'
 global.sessionName = './sessionye/haikal'
 global.prefa = ['','!','.',',','🐤','🗿']
 global.sp = '⭔'
